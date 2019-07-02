b11f4cd1d7340107e2aa120b5ec4502c072be5de
Emilio Garcia <egarcia@redhat.com>
2019-07-02 18:45:19 -0400

    Addressing comments: getopts, capitalization, usage

--- build_release_image.sh
+++ build_release_image.sh
@@ -1,101 +1,99 @@
+#!/bin/bash
+
+set -e
+
 help() {
-    echo "Build an Openshift Release Image"
-    echo "Usage:                    build_release_image.sh [options] <Images> <quay.io username>"
-    echo ""
-    echo "Options:"
-    echo "-h, --help                show this message"
-    echo "-t, --tag <tag>           push to a custom tag in your origin release image repo, default: mco"
-    echo ""
-    echo "Images: you must include at least one"
-    echo "-m, --mco                 build and push dependancies for mco to quay.io/<username>/image then"
-    echo "                              build a release image from that repo."
-    echo "-c, --capo [tag]          build a capo image locally, push it to quay.io/<username>/capo:[tag] then"
-    echo "                              build a release image from that repo."
+
+    cat <<EOM
+Build an Openshift Release Image"
+Usage:                      build_release_image.sh [options] <quay.io username>
+
+Options:
+-h, --help                  show this message
+-t, --tag <tag>             push to a custom tag in your origin release image repo, default: mco
+-m, --mco                   build and push dependancies for mco to quay.io/<username>/image then
+                                build a release image from that repo.
+-c, --capo <ocp certs>      build a capo image locally, push it to quay.io/<username>/capo then
+                                build a release image from that repo. You must pass the path to 
+                                a directory containing upstream ocp certs as an argument.
+EOM
+
+    exit 2
 }
 
-TAG="mco"
+TAG="custom"
 USERNAME=""
-CAPO=""
 MCO=""
-set -x
-# Parse Options
-while :; do
-    case $1 in
-        -h| --help )
-            help
-            exit 0
-            ;;
-        -t | --tag )
-            if [ -z "$2" ]; then
-                echo "Error: tag option passed without a tag value. Please use -h, or --help to see usage"
-                exit 1
-            fi
-            TAG=$2
-            ;;
-        -c | --capo )
-            if [ -z "$2" ]; then
-                CAPO="latest"
-            elif [[ $2 =~ ^\-.*|^\-\-.* ]];then
-                CAPO="latest"
-            else
-                CAPO="$2"
-            fi
-            ;;
-        -m | --mco )
-            MCO="true"
-            ;;
-        \? )
-            printf 'WARN: Unknown option : %s\n' "$1" >&2
-            exit 1
-            ;;
-        * )
-            shift
-            break
-            ;;
-    esac
+CAPO=""
 
+# Short Options
+SHORT=(
+    "h"
+    "t:"
+    "c:"
+    "m"
+)
+
+# Long Options
+LONG=(
+    "help"
+    "tag:"
+    "capo:"
+    "mco"
+)
+
+OPTS=$(getopt \
+    --options "$(printf "%s" "${SHORT[@]}")" \
+    --longoptions "$(printf "%s," "${LONG[@]}")" \
+    --name "$(basename "$0")" \
+    -- "$@"
+)
+eval set -- "$OPTS"
+
+# Parse Options
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -h| --help )  help;;
+        -t | --tag )  TAG=$2; shift;;
+        -c | --capo ) CAPO="$2"; shift;;
+        -m | --mco )  MCO="true";;
+        -- ) shift; break;;
+    esac
     shift
 done
 
-if [ -z "$CAPO" ] && [ -z "$MCO" ];then
-    echo "No images provided, can't build release image. Please use -h, or --help to see usage"
-    exit 1
-fi
-
 if [ -z "$1" ]; then
-    echo "No quay.io username provided, exiting ..."
+    echo "Error: No quay.io username provided"
     exit 1
 else
     USERNAME=$1
 fi
 
-RELEASEIMAGE="
+FROM_IMAGE="registry.svc.ci.openshift.org/origin/release:4.2"
+DEST_IMAGE="quay.io/$USERNAME/origin-release:$TAG"
+
+RELEASE_IMAGE="
 oc adm release new \
         --from-release="$FROM_IMAGE" \
         --to-image="$DEST_IMAGE" \
         --server https://api.ci.openshift.org \
         -n openshift"
 
-export DEST_IMAGE="quay.io/$USERNAME/origin-release:$TAG"
-export FROM_IMAGE="registry.svc.ci.openshift.org/origin/release:4.2"
-
 if [ -n "$CAPO" ]
 then
-    capoRepo=quay.io/$USERNAME/capo:$CAPO
+    CAPO_REPO=quay.io/$USERNAME/capo
     pushd $GOPATH/src/sigs.k8s.io/cluster-api-provider-openstack
-    podman build --no-cache -v $REPOS_DIR:/etc/yum.repos.d/:z -t $capoRepo .
-    podman push $capoRepo
+    podman build --no-cache -v $CAPO:/etc/yum.repos.d/:z -t $CAPO_REPO .
+    podman push $CAPO_REPO
     popd
 
-    RELEASEIMAGE="$RELEASEIMAGE \
-    openstack-machine-controllers=$capoRepo"
+    RELEASE_IMAGE="$RELEASE_IMAGE \
+    openstack-machine-controllers=$CAPO_REPO"
 fi
-
-pushd $GOPATH/src/github.com/openshift/machine-config-operator
 
 if [ -n "$MCO" ]
 then
-
+    pushd $GOPATH/src/github.com/openshift/machine-config-operator
     for image in controller daemon operator server; do
         export WHAT=machine-config-$image
         ./hack/build-image.sh
@@ -106,14 +104,13 @@ then
         export REPO="quay.io/$USERNAME"
         ./hack/push-image.sh
     done
+    popd
 
-    RELEASEIMAGE="$RELEASEIMAGE \
-    machine-config-operator=quay.io/$USERNAME/origin-machine-config-operator \
-    machine-config-controller=quay.io/$USERNAME/origin-machine-config-controller \
-    machine-config-daemon=quay.io/$USERNAME/origin-machine-config-daemon \
-    machine-config-server=quay.io/$USERNAME/origin-machine-config-server"
+    RELEASE_IMAGE="$RELEASE_IMAGE \
+    machine-config-operator=quay.io/$USERNAME/origin-machine-config-operator:latest \
+    machine-config-controller=quay.io/$USERNAME/origin-machine-config-controller:latest \
+    machine-config-daemon=quay.io/$USERNAME/origin-machine-config-daemon:latest \
+    machine-config-server=quay.io/$USERNAME/origin-machine-config-server:latest"
 fi
 
-$RELEASEIMAGE
-
-popd
+$RELEASE_IMAGE
