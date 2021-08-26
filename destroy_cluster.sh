5653dbbef9a4706128f6a99ccbca44a18c407dbd
Martin André <m.andre@redhat.com>
2021-08-26 14:55:14 +0200

    Also delete worker server group, if any

--- destroy_cluster.sh
+++ destroy_cluster.sh
@@ -99,6 +99,7 @@ if [[ $FORCE == true ]]; then
     openstack security group delete $INFRA_ID-worker
 
     openstack server group delete $INFRA_ID-master
+    openstack server group delete $INFRA_ID-worker
 
     for c in $(openstack container list -f value); do
         echo $c
