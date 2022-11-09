fa304c45bbd275fd70b85a450506e22af9d4ac21
Matthew Booth <mbooth@redhat.com>
2022-11-09 21:39:09 +0000

    Add reserved to returned metrics
    This will usually be zero, but if it started growing it would indicate
    an interesting problem in OpenStack.

--- metrics.sh
+++ metrics.sh
@@ -11,11 +11,12 @@ set -Eeuo pipefail
 os_project="$(openstack token issue -f value -c project_id)"
 
 for service in 'compute' 'network'; do
-	openstack quota list --detail "--${service}" --project "$os_project" -f value -c 'Resource' -c 'In Use' -c 'Limit' | while read -r resource inuse limit; do
+	openstack quota list --detail "--${service}" --project "$os_project" -f value -c 'Resource' -c 'In Use' -c 'Reserved' -c 'Limit' | while read -r resource inuse reserved limit; do
 		metric=openstack_quota_${service}_${resource}
 		echo "# HELP ${metric} OpenStack project quota for ${service} ${resource}"
 		echo "# TYPE ${metric} gauge"
 		echo "${metric}{cloud=\"${OS_CLOUD}\",project=\"${os_project}\",type=\"inuse\"} $inuse"
+		echo "${metric}{cloud=\"${OS_CLOUD}\",project=\"${os_project}\",type=\"reserved\"} $reserved"
 		echo "${metric}{cloud=\"${OS_CLOUD}\",project=\"${os_project}\",type=\"limit\"} $limit"
 	done
 done
