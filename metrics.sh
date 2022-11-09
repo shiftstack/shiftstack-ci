175f7c5dbfb4f1064db38310e70c905c2ddd3f96
Matthew Booth <mbooth@redhat.com>
2022-11-09 19:53:57 +0000

    Remove timestamp from metrics
    Prometheus will not use this timestamp except to ignore the data if it
    considers it too old. As we never want this we remove the timestamp.

--- metrics.sh
+++ metrics.sh
@@ -12,9 +12,8 @@ os_project="$(openstack token issue -f value -c project_id)"
 
 echo "# These metrics refer to project '$os_project' in the '$OS_CLOUD' cloud."
 for service in 'compute' 'network'; do
-	timestamp="$(date +"%s000")"
 	openstack quota list --detail "--${service}" --project "$os_project" -f value -c 'Resource' -c 'In Use' -c 'Limit' \
 		| sed -n \
-			-e 's/^\(\w\+\)\s\([[:digit:]]\+\)\s\([[:digit:]]\+\)$/'"$service"'_\1{quota="inuse"} \2 '"$timestamp"'\n'"$service"'_\1{quota="limit"} \3 '"$timestamp"'/gp;' \
-			-e 's/^\(\w\+\)\s\([[:digit:]]\+\)\s-1$/'"$service"'_\1{quota="inuse"} \2 '"$timestamp"'/gp'
+			-e 's/^\(\w\+\)\s\([[:digit:]]\+\)\s\([[:digit:]]\+\)$/'"$service"'_\1{quota="inuse"} \2\n'"$service"'_\1{quota="limit"} \3/gp;' \
+			-e 's/^\(\w\+\)\s\([[:digit:]]\+\)\s-1$/'"$service"'_\1{quota="inuse"} \2/gp'
 done
